e189f8afba88230222dc10f5a0c38ab265de3188
PhilippeHansen <HansenEstruchP0969@sduhsd.lan>
2018-06-05 08:26:39 -0700

    Update

---
+++ MyContactApp2018/MyContactApp2018/app/src/main/java/com/example/hansenestruchp0969/mycontactapp2018/activity_search.java
@@ -0,0 +1,13 @@
+package com.example.hansenestruchp0969.mycontactapp2018;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class activity_search extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_search);
+    }
+}
